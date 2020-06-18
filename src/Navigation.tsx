7cc295a19b120eefd4b63842920a8556a44a2377
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-06-18 20:40:26 +0200

    icon for launch pad

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -11,13 +11,14 @@ import {
 import { IconNames } from "@blueprintjs/icons";
 import glide from './Icons/glide.svg';
 import rocket from './Icons/rocket.svg';
+import launchpad from './Icons/launchpad.svg';
 
 export interface NavigationProps {}
 
 export class Navigation extends React.PureComponent<NavigationProps> {
   public render() {
     return <>
-      <Navbar>
+      <Navbar className={Classes.DARK}>
         <NavbarGroup align={Alignment.LEFT}>
           <NavbarHeading>
             <AnchorButton href="/" className="bp3-minimal" icon={<img src={glide} width={16} />} text="Glide">
@@ -25,7 +26,7 @@ export class Navigation extends React.PureComponent<NavigationProps> {
               </AnchorButton>
           </NavbarHeading>
           <NavbarDivider />
-          <AnchorButton href="/launchpad" className="bp3-minimal" icon={IconNames.LAYER} text="Launch Pad"  />
+          <AnchorButton href="/launchpad" className="bp3-minimal" icon={<img src={launchpad} width={16} />} text="Launch Pad"  />
           <AnchorButton href="/rocket" className="bp3-minimal" icon={<img src={rocket} width={16} />} text="Rocket" />
         </NavbarGroup>
       </Navbar>
