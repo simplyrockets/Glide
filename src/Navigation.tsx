ffdc955131c7b080c4cf4e3fef8aaa1f0b9936a6
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-06-18 20:31:42 +0200

    added some icons

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -9,20 +9,24 @@ import {
   AnchorButton
 } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
+import glide from './Icons/glide.svg';
+import rocket from './Icons/rocket.svg';
 
 export interface NavigationProps {}
 
 export class Navigation extends React.PureComponent<NavigationProps> {
   public render() {
     return <>
-      <Navbar className={Classes.DARK}>
+      <Navbar>
         <NavbarGroup align={Alignment.LEFT}>
           <NavbarHeading>
-            <AnchorButton href="/" className="bp3-minimal" icon={IconNames.HOME} text="Glide"  />
+            <AnchorButton href="/" className="bp3-minimal" icon={<img src={glide} width={16} />} text="Glide">
+
+              </AnchorButton>
           </NavbarHeading>
           <NavbarDivider />
           <AnchorButton href="/launchpad" className="bp3-minimal" icon={IconNames.LAYER} text="Launch Pad"  />
-          <AnchorButton href="/rocket" className="bp3-minimal" icon={IconNames.AIRPLANE} text="Rocket" />
+          <AnchorButton href="/rocket" className="bp3-minimal" icon={<img src={rocket} width={16} />} text="Rocket" />
         </NavbarGroup>
       </Navbar>
     </>
