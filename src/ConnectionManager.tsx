88c684cd9d794f797ecac906791daffe7fe2fdc3
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-06-21 23:51:53 +0200

    added connection manager component

---
+++ src/ConnectionManager.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { FormGroup, InputGroup, Dialog, Classes, Button, AnchorButton, Intent } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+
+export interface IProps {
+  endpoint?: string;
+  onSave: (endpoint: string) => void;
+}
+
+export const ConnectionManager = (props: IProps) => {
+  const [endpoint, setEndpoint] = useState<string>("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  function save() {
+    console.log('endpoint: ', endpoint);
+    props.onSave(endpoint);
+  }
+
+  return (<>
+    <Button
+      text="Connect"
+      intent="primary"
+      large
+      rightIcon={IconNames.CELL_TOWER}
+      onClick={() => setIsOpen(true)}
+    />
+    <Dialog icon={IconNames.CELL_TOWER} title="Connection Manager" isOpen={isOpen} onClose={() => setIsOpen(false)}>
+      <div className={Classes.DIALOG_BODY}>
+        <FormGroup
+          helperText=""
+          label="Server Endpoint"
+          labelFor="text-input"
+          labelInfo="(required)"
+        >
+          <InputGroup
+            id="endpoint"
+            placeholder="ws://server:3000/relay"
+            value={endpoint}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEndpoint(e.target.value)}
+          />
+        </FormGroup>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={() => setIsOpen(false)}>Cancel</Button>
+          <AnchorButton
+            intent={Intent.PRIMARY}
+            onClick={() => save()}
+          >
+            Save
+          </AnchorButton>
+        </div>
+      </div>
+    </Dialog>
+  </>)
+};
--- src/LaunchPad/Telemetry.tsx
+++ src/LaunchPad/Telemetry.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { NonIdealState, Button } from '@blueprintjs/core';
-import { IconNames } from '@blueprintjs/icons';
+import React from "react";
+import { NonIdealState } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+import { ConnectionManager } from "../ConnectionManager";
 
-export const Telemetry = () => <>
-  <NonIdealState
+export const Telemetry = () => (
+  <>
+    <NonIdealState
       icon={IconNames.ISSUE}
       title="Not connected to launch pad"
       description="Please establish connection first."
-      action={<Button text="Connect" intent="primary" large rightIcon={IconNames.CELL_TOWER} />}
-  />
-</>;
+      action={
+        <>
+          <ConnectionManager onSave={(endpoint) => {}} />
+        </>
+      }
+    />
+  </>
+);
