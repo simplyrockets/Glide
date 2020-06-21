373661dc2f97f0a1d0f22cb3c4d6ea4f88136a07
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-06-22 01:13:14 +0200

    update view with non-ideal state

--- src/analysis/LogAnalysis.tsx
+++ src/analysis/LogAnalysis.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { NonIdealState, Button } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 
 export const LogAnalysis = () => {
-  return (<></>);
+  return <>
+    <NonIdealState
+      icon={IconNames.CLOUD_UPLOAD}
+      title="Upload the log file to analyse"
+      description="Upload the log file, and analysis will happen."
+      action={<Button text="Upload" intent="primary" />}
+    />
+  </>;
 };
 
