8bb7fede4c2a619ebcd28fe100a3a2d1e6ba69f5
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-06-24 23:18:26 +0200

    change Mosiac key type

--- src/launchpad/Telemetry.tsx
+++ src/launchpad/Telemetry.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import { NonIdealState } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import ConnectionManager from "../ConnectionManager";
-import { Mosaic } from "react-mosaic-component";
+import { Mosaic, MosaicWindow } from "react-mosaic-component";
+import 'react-mosaic-component/react-mosaic-component.css'
+
+export type ViewId = 'a' | 'b' | 'c' | 'new';
 
 export default function Telemetry() {
   const ELEMENT_MAP: { [viewId: string]: JSX.Element } = {
     a: <div>Left Window</div>,
     b: <div>Top Right Window</div>,
     c: <div>Bottom Right Window</div>,
+  };
+
+  const TITLE_MAP: Record<ViewId, string> = {
+    a: 'Left Window',
+    b: 'Top Right Window',
+    c: 'Bottom Right Window',
+    new: 'New Window'
   };
 
   return (
@@ -20,19 +30,25 @@ export default function Telemetry() {
         action={<ConnectionManager onSave={(endpoint) => {}} />}
       />
 
-      <Mosaic<string>
-        renderTile={(id) => ELEMENT_MAP[id]}
-        initialValue={{
-          direction: "row",
-          first: "a",
-          second: {
-            direction: "column",
-            first: "b",
-            second: "c",
-          },
-          splitPercentage: 40,
-        }}
-      />
+      <div>
+        <Mosaic<ViewId>
+          renderTile={(id, path) => (
+            <MosaicWindow<ViewId> path={path} createNode={() => 'new'} title={TITLE_MAP[id]}>
+              <h1>{TITLE_MAP[id]}</h1>
+            </MosaicWindow>
+          )}
+          initialValue={{
+            direction: "row",
+            first: 'a',
+            second: {
+              direction: 'column',
+              first: 'b',
+              second: 'c',
+            },
+            splitPercentage: 40,
+          }}
+        />
+      </div>
     </>
   );
 }
