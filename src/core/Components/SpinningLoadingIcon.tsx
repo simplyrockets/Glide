db74397d290b7c255417944ceb406b16d92de732
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-06-25 23:21:08 +0200

    added alt attrib

--- src/core/Components/SpinningLoadingIcon.tsx
+++ src/core/Components/SpinningLoadingIcon.tsx
@@ -5,7 +5,7 @@ import styles from './SpinningLoadingIcon.module.scss';
 
 export default class SpinningLoadingIcon extends PureComponent<{}> {
   render() {
-    return <img src={LoadingIcon} className={styles.spin} />
+    return <img src={LoadingIcon} className={styles.spin} alt="..." />
   }
 }
 
