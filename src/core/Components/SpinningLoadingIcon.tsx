a78683572cd64a2f13b6745e39e9a85c7473687a
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-06-25 23:16:12 +0200

    added spinning loading icon

---
+++ src/core/Components/SpinningLoadingIcon.tsx
@@ -0,0 +1,11 @@
+import LoadingIcon from '@mdi/svg/svg/loading.svg';
+import React, { PureComponent } from 'react';
+
+import styles from './SpinningLoadingIcon.module.scss';
+
+export default class SpinningLoadingIcon extends PureComponent<{}> {
+  render() {
+    return <img src={LoadingIcon} className={styles.spin} />
+  }
+}
+
