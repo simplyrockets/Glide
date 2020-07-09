792b746c028f610aa70d83909fbcfbb613456d45
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-07-09 17:22:26 +0200

    formatting

--- src/core/panels/PanelList/PanelList.tsx
+++ src/core/panels/PanelList/PanelList.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { flatten } from "lodash";
-import { panelsByCategory as pbc } from "../../panelLoader";
-import { PanelComponentType } from "core/components/Panel";
+import React from 'react';
+import { flatten } from 'lodash';
+import { panelsByCategory as pbc } from '../../panelLoader';
+import { PanelComponentType } from 'core/components/Panel';
 
 export type PanelListItem = {
-  title?: string,
-  component?: PanelComponentType<any>
-}
+  title?: string;
+  component?: PanelComponentType<any>;
+};
 
 function getPanelsByCategory(): { [category: string]: PanelListItem[] } {
   const panelsByCategory: any = pbc();
@@ -17,7 +17,11 @@ function getPanelsByCategory(): { [category: string]: PanelListItem[] } {
 export class PanelList extends React.Component<{}, { searchQuery: string }> {
   static getComponentForType(type: string): PanelComponentType<any> | void {
     const panelsByCategory = getPanelsByCategory();
-    const allPanels = flatten(Object.keys(panelsByCategory).map((category) => panelsByCategory[category]));
+    const allPanels = flatten(
+      Object.keys(panelsByCategory).map(
+        (category) => panelsByCategory[category]
+      )
+    );
 
     const panel = allPanels.find((item) => {
       return item?.component?.panelType === type;
