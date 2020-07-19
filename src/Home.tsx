01dc0bd56b7cc2ada3bae82bacde050ccb98b6df
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-07-19 08:54:06 +0200

    add recoil

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
+
 import Navigation from 'Navigation';
 import PanelLayout from 'PanelLayout';
 import Timeline from 'Timeline';
 
 export default function Home() {
   return (
-    <>
+    <RecoilRoot>
       <Navigation />
       <PanelLayout />
       <Timeline />
-    </>
+    </RecoilRoot>
   );
 }
