409ff5ffa675869e6c3f1a0cf2beb28666f3bf9a
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-08-02 15:25:38 +0200

    chore(formatting)

--- src/core/panelLoader.tsx
+++ src/core/panelLoader.tsx
@@ -1,13 +1,13 @@
-import Note from "./panels/Note/Note";
-import Plot from "./panels/Plot/Plot";
-import StdOut from "./panels/StdOut/StdOut";
+import Note from './panels/Note/Note';
+import Plot from './panels/Plot/Plot';
+import StdOut from './panels/StdOut/StdOut';
 
 export function panelsByCategory() {
   const glide = [
-    { title: "Note", component: Note },
-    { title: "Plot", component: Plot }
+    { title: 'Note', component: Note },
+    { title: 'Plot', component: Plot }
   ];
-  const debug = [{ title: "Messages", component: StdOut }];
+  const debug = [{ title: 'Messages', component: StdOut }];
 
   return {
     glide,
@@ -16,5 +16,8 @@ export function panelsByCategory() {
 }
 
 export function panelCategories() {
-  return [{ label: "Glide", key: "glide" }, { label: "Debug", key: "debug" }];
+  return [
+    { label: 'Glide', key: 'glide' },
+    { label: 'Debug', key: 'debug' }
+  ];
 }
